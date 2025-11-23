71e49549fb5a992dca0d4f9b532d7da58170bcdf
Saxon-Pi <ts.8849.p@gmail.com>
2025-11-23 13:56:09 +0900

    create glue merge job stack

--- lib/glue-parquet-splitter-stack.ts
+++ lib/glue-parquet-splitter-stack.ts
@@ -4,15 +4,23 @@ import * as glue from 'aws-cdk-lib/aws-glue';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 
+// *****************************************************************************************
+// input の .parquet ファイルを分割する Glue job を作成するスタック
+// 入力データの 'item_id' ごとに個別のファイルを作成して split/<item_id> フォルダに格納する
+// 分割したファイルごとに markers ファイルを作成し、分割後のオブジェクト URI を記載している（削除処理で使用）
+// 処理済みのデータサンプルは sample-data フォルダに格納している
+// 入力ファイルのサイズに合わせて pyshell / ray の両方でジョブを作成している（処理は同じ）
+// *****************************************************************************************
+
 // 環境変数
 // python スクリプトは pyshell, ray 共通、入出力と完了マーカーは別 prefix に格納
 const bucketName           = "glue-split-job-saxon";  // S3バケット名
 const scriptName           = "parquet-splitter.py";   // Glue で実行するスクリプト名
-const scriptPrefix         = "src/glue/";             // スクリプトの S3 prefix
+const scriptPrefix         = "src/glue/split/";        // スクリプトの S3 prefix
 const inPrefixPyshell      = "data/input/pyshell";    // 入力データの S3 prefix (Pyshell)
 const inPrefixRay          = "data/input/ray";        // 入力データの S3 prefix (Ray)
-const outPrefixPyshell     = "data/output/pyshell";   // 出力データの S3 prefix (Pyshell)
-const outPrefixRay         = "data/output/ray";       // 出力データの S3 prefix (Ray)
+const outPrefixPyshell     = "data/split/pyshell";    // 出力データの S3 prefix (Pyshell)
+const outPrefixRay         = "data/split/ray";        // 出力データの S3 prefix (Ray)
 const markerPrefixPyshell  = "data/markers/pyshell";  // マーカー（分割情報）の S3 prefix (Pyshell)
 const markerPrefixRay      = "data/markers/ray";      // マーカー（分割情報）の S3 prefix (Ray)
 
